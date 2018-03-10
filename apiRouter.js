7931e0b975c390be48e475208a4714d108ed144c
Dimitri Dessus <iphoneretro@icloud.com>
2018-03-10 15:54:04 +0100

    [Add] Messages routes

--- apiRouter.js
+++ apiRouter.js
@@ -1,6 +1,7 @@
 // Imports
-var express   = require('express');
-var usersCtrl = require('./routes/usersCtrl');
+var express      = require('express');
+var usersCtrl    = require('./routes/usersCtrl');
+var messagesCtrl = require('./routes/messagesCtrl');
 
 // Router
 exports.router = (function() {
@@ -11,6 +12,10 @@ exports.router = (function() {
   apiRouter.route('/users/login/').post(usersCtrl.login);
   apiRouter.route('/users/me/').get(usersCtrl.getUserProfile);
   apiRouter.route('/users/me/').put(usersCtrl.updateUserProfile);
+
+  // Messages routes
+  apiRouter.route('/messages/new/').post(messagesCtrl.createMessage);
+  apiRouter.route('/messages/').get(messagesCtrl.listMessages);
 
   return apiRouter;
 })();
