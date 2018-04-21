a308c512898fe29092d2f62090f38c3bea3ff539
Dimitri Dessus <iphoneretro@icloud.com>
2018-04-21 12:31:32 +0200

    [Add] Creating like table

---
+++ migrations/20180421094946-create-like.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Likes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      messageId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Messages',
+          key: 'id'
+        }
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Likes');
+  }
+};
---
+++ models/like.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Like = sequelize.define('Like', {
+    messageId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Message',
+        key: 'id'
+      }
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'User',
+        key: 'id'
+      }
+    }
+  }, {});
+  Like.associate = function(models) {
+    // associations can be defined here
+
+    models.User.belongsToMany(models.Message, {
+      through: models.Like,
+      foreignKey: 'userId',
+      otherKey: 'messageId',
+    });
+
+    models.Message.belongsToMany(models.User, {
+      through: models.Like,
+      foreignKey: 'messageId',
+      otherKey: 'userId',
+    });
+
+    models.Like.belongsTo(models.User, {
+      foreignKey: 'userId',
+      as: 'user',
+    });
+
+    models.Like.belongsTo(models.Message, {
+      foreignKey: 'messageId',
+      as: 'message',
+    });
+  };
+  return Like;
+};
